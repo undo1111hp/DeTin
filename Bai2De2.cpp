4c8462af11e8a17ed90afb92cdd53b76b9a4ac8e
undo1111hp <108620075+undo1111hp@users.noreply.github.com>
2022-10-17 23:22:36 +0700

    Add files via upload

---
+++ Bai2De2.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool sto(int n){
+    if (n <= 1)
+        return false;
+    for (int i = 2; i < n; i++){
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main(){
+	int n;
+	cin >> n;
+	for(int i = 0; i < n/2; i++){
+		for(int j = n; i > n/2; j--){
+			if(sto(i) == true && sto(j) == true){
+					cout << i << ";" << j << endl;
+			}
+		}
+	}
+}
